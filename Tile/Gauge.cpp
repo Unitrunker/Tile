ef56890519a4a4130d77394c647196c4ed4933e8
Rick Parrish <unitrunker@gmail.com>
2012-10-13 00:27:21 -0500

    Gauge beginnings.

---
+++ Tile/Gauge.cpp
@@ -0,0 +1,104 @@
+#include "stdafx.h"
+#include "Gauge.h"
+#include "Flow.h"
+#include "ICanvas.h"
+#include "../JSON/Writer.h"
+
+/*
+Copyright © 2012 Rick Parrish
+*/
+
+using namespace Tiles;
+
+Gauge::Gauge(identity_t id, Theme& theme, orient_t flow) :
+	Tile(id, theme),
+	_flow(flow),
+	_value(0),
+	_thick(2)
+{
+	FlowDesc desc = {1, 1, 0, true};
+	setFlow(eRight, desc);
+	setFlow(eDown, desc);
+	_fore.color = _back.color = 0;
+	_fore.index = Theme::eCaptionFore;
+	_back.index = Theme::eCaptionBack;
+}
+
+// instance type
+const char* Gauge::getType() const
+{
+	return "Gauge";
+}
+
+const char* Gauge::type()
+{
+	return "Gauge";
+}
+
+/// <param name="canvas">Canvas where text will appear.</param>
+bool Gauge::Draw(ICanvas *canvas, bool bFocus)
+{
+	bFocus; // ignored, will always be false.
+
+	rect_t rect;
+	getRect(rect);
+
+	rect_t left, right, center;
+
+	left = rect;
+	right = rect;
+	center = rect;
+
+	if (_flow == eRight || _flow == eLeft)
+	{
+		left.wide = _value;
+		if (left.wide > rect.wide)
+			left.wide = rect.wide;
+		center.x = left.right();
+		center.wide = _thick;
+		right.x = center.right();
+		right.wide = rect.wide - left.wide - center.wide;
+	}
+	else
+	{
+		left.high = _value;
+		if (left.high > rect.high)
+			left.high = rect.high;
+		center.y = left.bottom();
+		center.high = _thick;
+		right.y = center.bottom();
+		right.high = rect.high - left.high - center.high;
+	}
+
+	color_t fore = _theme.getColor(_fore);
+	color_t back = _theme.getColor(_back);
+	canvas->FillRectangle(left, back);
+	canvas->FillRectangle(center, fore);
+	canvas->FillRectangle(right, back);
+	_bChanged = false;
+	return true;
+}
+
+// serialize
+bool Gauge::save(JSON::Writer &writer)
+{
+	writer.writeStartObject();
+	writer.writeNamedValue("type", type());
+	Tile::save(writer);
+	writer.writeNamedValue("orient", Flow::getName(_flow));
+	writer.writeEndObject(true);
+	return true;
+}
+
+const meter_t &Gauge::getValue() const
+{
+	return _value;
+}
+
+bool Gauge::setValue(const meter_t &value)
+{
+	bool bChange = _value != value;
+	_value = value;
+	setChanged(bChange);
+	return true;
+}
---
+++ Tile/Gauge.h
@@ -0,0 +1,68 @@
+#include "Tile.h"
+#include "Theme.h"
+#include "Accessor.h"
+
+/*
+Copyright © 2011 Rick Parrish
+*/
+
+#pragma once
+
+namespace Tiles
+{
+
+struct Gauge : public Tile, public IAccessor<meter_t>
+{
+  	Gauge(identity_t id, Theme& theme, orient_t flow);
+	virtual ~Gauge() { };
+    /// <param name="canvas">Canvas where text will appear.</param>
+	virtual bool Draw(ICanvas *canvas, bool bFocus);
+	// get/set accessors for changed flag.
+	using Tile::getChanged;
+	using Tile::setChanged;
+	// set callback for change notification.
+	using Tile::watch;
+
+	using Tile::identity;
+	// instance type
+	virtual const char* getType() const;
+	using Tile::getMin;
+	using Tile::setMin;
+	// get/set accessors for layout maximums
+	using Tile::getMax;
+	using Tile::setMax;
+	// get/set accessors for layout weights
+	using Tile::getWeight;
+	using Tile::setWeight;
+	// get/set accessors for layout descriptors
+	using Tile::getFlow;
+	using Tile::setFlow;
+	// get/set accessor for bounding rectangle.
+	using Tile::setRect;
+	using Tile::getRect;
+	// accessors for scrollable area.
+	using Tile::getScrollBox;
+	using Tile::setScrollBox;
+	using Tile::getScrollArea;
+	using Tile::setScrollArea;
+	using Tile::getScrollPoint;
+	using Tile::setScrollPoint;
+
+	using Tile::contains;
+	// serialize
+	virtual bool save(JSON::Writer &writer);
+	static const char* type();
+
+	virtual const meter_t &getValue() const;
+	virtual bool setValue(const meter_t &value);
+
+	Theme::Color _fore;
+	Theme::Color _back;
+private:
+	// orientation of gauge
+	orient_t _flow;
+	meter_t _value;
+	meter_t _thick;
+};
+
+};
