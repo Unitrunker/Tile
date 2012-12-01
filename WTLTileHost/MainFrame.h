3854fd8b060b4b487ed53b517fb9d41c5bf8c822
Rick Parrish <unitrunker@gmail.com>
2012-12-01 00:25:53 -0600

    Click event arglist changed.

--- WTLTileHost/MainFrame.h
+++ WTLTileHost/MainFrame.h
@@ -8,13 +8,14 @@
 struct MainFrame : public Frame
 {
 	MainFrame(Theme &theme);
-	void clickTheme(Button *, bool bDown);
-	void clickAbout(Button *, bool bDown);
-	void clickOK(Button *, bool);
-	void clickSnapshot(Button *, bool);
-	// user clicked check
-	void clickTick(Button *, bool bDown);
 private:
+	void clickTheme(Button *);
+	void clickAbout(Button *);
+	void clickOK(Button *);
+	void clickSnapshot(Button *);
+	// user clicked check
+	void clickTick(Button *);
+
 	Window m_editor;
 	Window m_about;
 	ThemeSet m_set;
